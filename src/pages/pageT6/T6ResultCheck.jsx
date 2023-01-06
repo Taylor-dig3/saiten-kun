6165da1ec5374d85bb6b2642071758ec96064a72
KawaguchiRion <riiiiion@gmail.com>
2023-01-06 13:40:10 +0900

    test

--- src/pages/pageT6/T6ResultCheck.jsx
+++ src/pages/pageT6/T6ResultCheck.jsx
@@ -29,6 +29,8 @@ export default function T6ResultCheck() {
   const [selectStudent, setSelectStudent] = useState();
   const [paper, setPaper] = useState();
   const [tablePaper, setTablePaper] = useState();
+  const [score, setScore] = useState();
+
   const navigate = useNavigate();
   const t5TestListDisplay = () => {
     navigate("../T5TestList");
@@ -66,10 +68,10 @@ export default function T6ResultCheck() {
   };
 
   const selectStudentChange = (e) => {
-    console.log(e.target)
-    console.log(e.target.value.split("&%",2))
-    const valueArr = e.target.value.split("&%",2)
-    const studentInfo = {id:Number(valueArr[0]),name:valueArr[1]}
+    console.log(e.target);
+    console.log(e.target.value.split("&%", 2));
+    const valueArr = e.target.value.split("&%", 2);
+    const studentInfo = { id: Number(valueArr[0]), name: valueArr[1] };
     setSelectStudent(studentInfo);
     axios
       .get("/answer", {
@@ -108,6 +110,11 @@ export default function T6ResultCheck() {
     if (paper) {
       console.log("object");
       console.log(paper);
+
+      const questionCount = paper.data.length;
+      const correctCount = paper.data.filter((elem) => elem.result).length;
+      console.log((correctCount / questionCount) * 100);
+      setScore(Math.round((correctCount / questionCount) * 100));
       setTablePaper(
         paper.data.map((elem, index) => {
           console.log(elem);
@@ -119,9 +126,10 @@ export default function T6ResultCheck() {
             <TableRow key={index}>
               <TableCell>{elem["question"]}</TableCell>
               <TableCell>{elem["answer"]}</TableCell>
-              <TableCell><img className="answer" src={decodedFile} alt="answer" />
+              <TableCell>
+                <img className="answer" src={decodedFile} alt="answer" />
               </TableCell>
-              <TableCell >
+              <TableCell>
                 {elem["result"] ? (
                   <img className="T6marubatsu" src="./img/maru.png" alt="" />
                 ) : (
@@ -132,11 +140,13 @@ export default function T6ResultCheck() {
                 <button
                   onClick={() => {
                     const flag = elem.result ? false : true;
-                    axios.put("/answer",{
-                      result_id:elem.result_id
-                    }).then(res=>{
-                      console.log(res)
-                    })
+                    axios
+                      .put("/answer", {
+                        result_id: elem.result_id,
+                      })
+                      .then((res) => {
+                        console.log(res);
+                      });
                     setPaper((prev) => {
                       console.log(prev);
                       prev.data.splice(index, 1, {
@@ -154,13 +164,13 @@ export default function T6ResultCheck() {
             </TableRow>
           );
         })
-      );
-    }
-  }, [paper]);
-
-  return (
-    <div>
-      {/* <h2 className="title">{title}</h2> */}
+        );
+      }
+    }, [paper]);
+    
+    return (
+      <div>
+      <h1 className="T6titleqqqq">{title}</h1>
       <div className="studentsSelect">
         <Select
           labelId="demo-controlled-open-select-label"
@@ -172,7 +182,6 @@ export default function T6ResultCheck() {
           label="Age"
           onChange={gradeChange}
         >
-          {/* <MenuItem value={"学年を選択してください"}>学年を選択してください</MenuItem> */}
           <MenuItem value={"全学年"}>全学年</MenuItem>
           <MenuItem value={1}>1年生</MenuItem>
           <MenuItem value={2}>2年生</MenuItem>
@@ -184,10 +193,6 @@ export default function T6ResultCheck() {
         <Select
           labelId="demo-controlled-open-select-label"
           id="demo-controlled-open-select"
-          // open={studentListOpen}
-          // onClose={studentListHandleClose}
-          // onOpen={studentListHandleOpen}
-          // value={selectStudent || 0}
           defaultValue={0}
           label="Age"
           onChange={selectStudentChange}
@@ -196,46 +201,52 @@ export default function T6ResultCheck() {
           {filterStudentIdList
             ? filterStudentIdList.map((elem, index) => {
                 return (
-                  <MenuItem key={elem.id} value={`${elem.id}&%${elem.name}`} >{`ID:${(
-                    "0000" + elem.id
-                  ).slice(-4)}　　　 ${elem.name}`}</MenuItem>
-                );
-              })
-            : 1}
+                  <MenuItem
+                  key={elem.id}
+                  value={`${elem.id}&%${elem.name}`}
+                  >{`ID:${("0000" + elem.id).slice(-4)}　 ${
+                    elem.name
+                  }`}</MenuItem>
+                  );
+                })
+                : 1}
         </Select>
-        </div>
-        {selectStudent ?(
-<>
+      </div>
+      {selectStudent ? (
+        <>
           <span className="T6StudentsID" value="ID">
-          ID:{selectStudent.id}   
-        </span>
-        <span className="StudentsName" >
-          名前:{selectStudent.name}
-        </span>
-      <span className="score">{}</span>
-      <span className="scoreUnit">点</span>
-      <button onClick={t5TestListDisplay}>戻る</button>
-      
-      <Container maxWidth="95%">
-        <TableContainer component={Paper} sx={{ maxHeight: "95%" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell style={{ width: "40%" }}>問題</TableCell>
-                <TableCell style={{ width: "25%" }}>正解</TableCell>
-                <TableCell style={{ width: "15%" }}>解答</TableCell>
-                <TableCell style={{ width: "10%" }}></TableCell>
-                <TableCell style={{ width: "10%" }}>正誤修正</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>{tablePaper || 1}</TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
+            ID:{("0000" + selectStudent.id).slice(-4)}
+          </span>
+          <span className="StudentsName">名前:{selectStudent.name}</span>
+          <span className="score">{}</span>
+          <span className="scoreUnit">{score}点</span>
+          <button onClick={t5TestListDisplay}>戻る</button>
+
+          <Container maxWidth="95%">
+            <TableContainer component={Paper} sx={{ maxHeight: "95%" }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ width: "40%" }}>問題</TableCell>
+                    <TableCell style={{ width: "25%" }}>正解</TableCell>
+                    <TableCell
+                      style={{ width: "15%" }}
+                      colSpan={2}
+                      align="center"
+                    >
+                      解答{" "}
+                    </TableCell>
+                    <TableCell style={{ width: "10%" }}>正誤修正</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>{tablePaper}</TableBody>
+              </Table>
+            </TableContainer>
+          </Container>
         </>
-        )
-        :
-        ("生徒を選択してください")}
+      ) : (
+        <h1>生徒が選択されていません</h1>
+      )}
     </div>
   );
 }
