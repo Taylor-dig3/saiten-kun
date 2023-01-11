df7e58bc4611839a76445e8c65fd7b57ea6ce55f
KawaguchiRion <riiiiion@gmail.com>
2023-01-11 20:11:43 +0900

    deploy

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -4,23 +4,21 @@
 # # - npm run migrate
 # # - npm run seed
 # npm install
-
-
 #!/bin/bash
-
 source ~/.bashrc
 source ~/.bash_profile
-
 node -v
 echo $PATH
-
-echo "diridou"
+echo “diridou”
 cd /var/www/html/
 pwd
-
+sudo chmod 777 package-lock.json
+echo “echo npm install”
 npm install
-npm run migrate
-
-echo "after install collect"
+# npm run migrate
+echo “after install collect”
 # systemctl start httpd
 # systemctl enable httpd
+#chmod 777 package-lock.json #r
+#chmod 777 package.json #r
+#chmod 777 build
