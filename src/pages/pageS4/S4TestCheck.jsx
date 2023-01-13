8dcbd4066c9400e9b795cfed21e46ee014c4786f
Terasaka <yoshinobu_terasaka@mail.toyota.co.jp>
2023-01-13 13:49:10 +0900

    S4 漢字表記修正

--- src/pages/pageS4/S4TestCheck.jsx
+++ src/pages/pageS4/S4TestCheck.jsx
@@ -29,7 +29,7 @@ const rewardObj = {
   elementCount: 500,
   elementSize: 15,
   zIndex: 4,
-  onAnimationComplete:()=>{
+  onAnimationComplete: () => {
     const elem = document.querySelector(".S4-baraemon-visible")
     elem.className = "S4-baraemon-hidden"
   }
@@ -48,69 +48,76 @@ export default function T6ResultCheck() {
     navigate("../S3ResultList");
   };
 
-    let  S3Title = "いままでやったテスト";
-    let  S3Back = "もどる";
-    let  S3Result = "テストけっかかくにん";
-    let  S3Sub = "きょうか";
-    let  S3Problem = "もんだいすう";
-    let  S3Score = "てんすう";
+  let S4Name = "";
+  let S4Back = "";
+  let S4Result = "";
+  let S4Num = "";
+  let S4Problem = "";
+  let S4Score = "";
+  let S4Ans = "";
 
   switch (loginInfo.grade) {
     case 1:
-      S3Title = "いままでやったテスト";
-      S3Back = "もどる";
-      S3Result = "テストけっかかくにん";
-      S3Sub = "きょうか";
-      S3Problem = "もんだいすう";
-      S3Score = "てんすう";
+      S4Name = "なまえ";
+      S4Back = "もどる";
+      S4Result = "テストけっかかくにん";
+      S4Num = "すう";
+      S4Problem = "もんだい";
+      S4Score = "てん";
+      S4Ans = "こたえ";
       break;
     case 2:
-      S3Title = "今までやったテスト";
-      S3Back = "もどる";
-      S3Result = "テストけっかかくにん";
-      S3Sub = "教科";
-      S3Problem = "もんだい数";
-      S3Score = "点数";
+      S4Name = "名前";
+      S4Back = "もどる";
+      S4Result = "テストけっかかくにん";
+      S4Num = "数";
+      S4Problem = "もんだい";
+      S4Score = "点";
+      S4Ans = "答え";
       break;
     case 3:
-      S3Title = "今までやったテスト";
-      S3Back = "もどる";
-      S3Result = "テストけっかかくにん";
-      S3Sub = "教科";
-      S3Problem = "問題数";
-      S3Score = "点数";
+      S4Name = "名前";
+      S4Back = "もどる";
+      S4Result = "テストけっかかくにん";
+      S4Num = "数";
+      S4Problem = "問題";
+      S4Score = "点";
+      S4Ans = "答え";
       break;
     case 4:
-      S3Title = "メニュー";
-      S3Back = "もどる";
-      S3Result = "テスト結果かくにん";
-      S3Sub = "教科";
-      S3Problem = "問題数";
-      S3Score = "点数";
+      S4Name = "名前";
+      S4Back = "もどる";
+      S4Result = "テスト結果かくにん";
+      S4Num = "数";
+      S4Problem = "問題";
+      S4Score = "点";
+      S4Ans = "答え";
       break;
     case 5:
-      S3Title = "過去のテスト";
-      S3Back = "もどる";
-      S3Result = "テスト結果確にん";
-      S3Sub = "教科";
-      S3Problem = "問題数";
-      S3Score = "点数";
+      S4Name = "名前";
+      S4Back = "もどる";
+      S4Result = "テスト結果確にん";
+      S4Num = "数";
+      S4Problem = "問題";
+      S4Score = "点";
+      S4Ans = "解答";
       break;
     case 6:
-      S3Title = "過去テスト一覧";
-      S3Back = "もどる";
-      S3Result = "テスト結果確認";
-      S3Sub = "教科";
-      S3Problem = "問題数";
-      S3Score = "点数";
+      S4Name = "名前";
+      S4Back = "もどる";
+      S4Result = "テスト結果確認";
+      S4Num = "数";
+      S4Problem = "問題";
+      S4Score = "点";
+      S4Ans = "解答";
       break;
   }
 
   useEffect(() => {
-   if(loginInfo.loginState === "notYetLoggedIn"){
-    navigate("../")
-   }
-    }, []);
+    if (loginInfo.loginState === "notYetLoggedIn") {
+      navigate("../")
+    }
+  }, []);
 
   useEffect(() => {
     if (paper !== false && paper !== undefined) {
@@ -129,7 +136,7 @@ export default function T6ResultCheck() {
       if (!resultNullFlag) {
         setScore(Math.round((correctCount / questionCount) * 100));
       } else {
-        setScore("未採");
+        setScore("＊＊ ");
       }
       setTablePaper(
         paper.data.map((elem, index) => {
@@ -147,9 +154,11 @@ export default function T6ResultCheck() {
               <TableCell>
                 {elem["result"] ? (
                   <img className="S4marubatsu" src="./img/maru.png" alt="" />
+                ) : (elem["result"] === null ? (
+                  <></>
                 ) : (
                   <img className="S4marubatsu" src="./img/batsu.png" alt="" />
-                )}
+                ))}
               </TableCell>
             </TableRow>
           );
@@ -172,27 +181,27 @@ export default function T6ResultCheck() {
         console.log(res.data);
         setPaper(res.data);
       });
-    }, []);
-    
+  }, []);
+
   useEffect(() => {
     if (score === 100) {
       console.log("score入ったよ");
-      
+
       const elem = document.querySelector(".S4-baraemon-hidden")
       elem.className = "S4-baraemon-visible"
-      setTimeout(()=>{
+      setTimeout(() => {
         reward();
-      },500)
-      
+      }, 500)
+
     }
   }, [score]);
-  
+
   return (
     <div className="S4-container">
-      <h1 className="S4-title">結果確認</h1>
+      <h1 className="S4-title">{S4Result}</h1>
       <div className="S4-button-container">
         <button onClick={s3TestListDisplay} className="S4-button">
-          戻る
+          {S4Back}
         </button>
       </div>
       <div className="S4-test-result-info">
@@ -200,13 +209,13 @@ export default function T6ResultCheck() {
           <span className="T6StudentsID" value="ID">
             ID : {("0000" + loginInfo.userId).slice(-4)}
           </span>
-          <span className="S4-student-name">名前 : {loginInfo.name}</span>
+          <span className="S4-student-name">{S4Name} : {loginInfo.name}</span>
         </div>
         <div className="S4-test-info">
           <span>{selectTestInfo.title}</span>
-          <span>問題数:{selectTestInfo.question_count}</span>
-          <span>テストの日:{createDate(selectTestInfo.run_date)}</span>
-          <span className="S4-test-score">{score}点</span>
+          <span>{S4Problem}{S4Num} : {selectTestInfo.question_count}</span>
+          <span>テストの日 : {createDate(selectTestInfo.run_date)}</span>
+          <span className="S4-test-score">{score} {S4Score}</span>
         </div>
       </div>
       {paper !== false ? (
@@ -216,13 +225,13 @@ export default function T6ResultCheck() {
             <Table stickyHeader aria-label="sticky table">
               <TableHead>
                 <TableRow>
-                  <TableCell style={{ width: "50%" }}>問題</TableCell>
+                  <TableCell style={{ width: "50%" }}>{S4Problem}</TableCell>
                   <TableCell
                     style={{ width: "50%" }}
                     colSpan={2}
                     align="center"
                   >
-                    解答{" "}
+                    {S4Ans}{" "}
                   </TableCell>
                 </TableRow>
               </TableHead>
@@ -232,14 +241,14 @@ export default function T6ResultCheck() {
         </Container>
       ) : (
         // </>
-        <h1 className="S4-mizissi">未実施</h1>
+        <h1 className="S4-mizissi">まだやってないよ</h1>
       )}
       <div className="S4-baraemon-hidden" >
-      <div id="rewardId">
-      <img src="./Bara-emon.PNG"  className="S4-baraemon-img"/>
-      </div>
+        <div id="rewardId">
+          <img src="./Bara-emon.PNG" className="S4-baraemon-img" />
         </div>
-      
+      </div>
+
     </div>
   );
 }
